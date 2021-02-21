a700f4f9708bc5a00a1919b3bd4f3162b3e03b81
James Smith <metal.lunchbox@gmail.com>
2021-02-21 12:44:03 +0800

    yet more cors debugging

--- src/shared/check-origin.js
+++ src/shared/check-origin.js
@@ -1,6 +1,12 @@
 module.exports.checkOrigin = origin => {
+  console.log('origin is:', origin)
+  console.log('process.env.ALLOWED_ORIGINS:', process.evn.ALLOWED_ORIGINS)
   if (!process.env.ALLOWED_ORIGINS) return false
   if (process.env.ALLOWED_ORIGINS === '*') return '*'
   const allowedOrigins = process.env.ALLOWED_ORIGINS.split(' ')
-  return allowedOrigins.includes(origin) && origin
+  console.log('allowOrigins is:', allowedOrigins)
+  console.log('includes?', allowedOrigins.includes(origin))
+  const value = allowedOrigins.includes(origin) && origin
+  console.log('checkOrigin will return:', value)
+  return value
 }
